5eee9395e3ef32e349630e91b721f7e9cce1825f
변가원 <byeongawon@byeongawon-ui-noteubug.local>
2024-06-04 10:18:07 +0900

    first commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,59 +1,104 @@
-import React, {useState} from 'react';
-import EditTask from '../modals/EditTask'
+import React, { useState } from "react";
+import EditTask from "../modals/EditTask";
+import { Card as MUICard, Typography, Checkbox, Button } from "@mui/material";
 
-const Card = ({taskObj, index, deleteTask, updateListArray}) => {
-    const [modal, setModal] = useState(false);
+const Card = ({ taskObj, index, deleteTask, updateListArray }) => {
+  const [modal, setModal] = useState(false);
 
-    const colors = [
-        {
-            primaryColor : "#5D93E1",
-            secondaryColor : "#ECF3FC"
-        },
-        {
-            primaryColor : "#F9D288",
-            secondaryColor : "#FEFAF1"
-        },
-        {
-            primaryColor : "#5DC250",
-            secondaryColor : "#F2FAF1"
-        },
-        {
-            primaryColor : "#F48687",
-            secondaryColor : "#FDF1F1"
-        },
-        {
-            primaryColor : "#B964F7",
-            secondaryColor : "#F3F0FD"
-        }
-    ]
+  const colors = [
+    {
+      primaryColor: "#5D93E1",
+      secondaryColor: "#ECF3FC",
+    },
+    {
+      primaryColor: "#F9D288",
+      secondaryColor: "#FEFAF1",
+    },
+    {
+      primaryColor: "#5DC250",
+      secondaryColor: "#F2FAF1",
+    },
+    {
+      primaryColor: "#F48687",
+      secondaryColor: "#FDF1F1",
+    },
+    {
+      primaryColor: "#B964F7",
+      secondaryColor: "#F3F0FD",
+    },
+  ];
 
-    const toggle = () => {
-        setModal(!modal);
-    }
+  const toggle = () => {
+    setModal(!modal);
+  };
 
-    const updateTask = (obj) => {
-        updateListArray(obj, index)
-    }
+  const updateTask = (obj) => {
+    updateListArray(obj, index);
+  };
 
-    const handleDelete = () => {
-        deleteTask(index)
-    }
+  const handleDelete = () => {
+    deleteTask(index);
+  };
 
-    return (
-        <div class = "card-wrapper mr-5">
-            <div class = "card-top" style={{"background-color": colors[index%5].primaryColor}}></div>
-            <div class = "task-holder">
-                <span class = "card-header" style={{"background-color": colors[index%5].secondaryColor, "border-radius": "10px"}}>{taskObj.Name}</span>
-                <p className = "mt-3">{taskObj.Description}</p>
+  const handleCheckbox = () => {
+    let tasks = { ...taskObj };
+    tasks.completed = !tasks.completed;
+    updateListArray(tasks, index);
+  };
 
-                <div style={{"position": "absolute", "top":"160px", "left":"160px"}}>
-                    <button style={{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {() => setModal(true)}>close</button>
-                    <button style = {{"color" : colors[index%5].primaryColor, "cursor" : "pointer"}} onClick = {handleDelete}>Delete</button>
-                </div>
+  return (
+    <MUICard className="card-wrapper mr-5">
+      <div
+        className="card-top"
+        style={{ backgroundColor: colors[index % 5].primaryColor }}
+      ></div>
+      <div
+        className="task-holder"
+        style={{ textDecoration: taskObj.completed ? "line-through" : "none" }}
+      >
+        <span
+          className="card-header"
+          style={{
+            backgroundColor: colors[index % 5].secondaryColor,
+            borderRadius: "10px",
+          }}
+        >
+          {taskObj.Name}
+        </span>
+        <Typography className="mt-3">{taskObj.Description}</Typography>
+
+        <div className="checkbox-container">
+          <Checkbox
+            type="checkbox"
+            checked={taskObj.completed}
+            onChange={handleCheckbox}
+          />
+          <label>완료</label>
         </div>
-        <EditTask modal = {modal} toggle = {toggle} updateTask = {updateTask} taskObj = {taskObj}/>
+
+        <div style={{ position: "absolute", top: "160px", left: "160px" }}>
+          <Button
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
+            onClick={() => setModal(true)}
+          >
+            edit
+          </Button>
+          <Button
+            style={{ color: colors[index % 5].primaryColor, cursor: "pointer" }}
+            onClick={handleDelete}
+          >
+            delete
+          </Button>
         </div>
-    );
+      </div>
+      <EditTask
+        modal={modal}
+        toggle={toggle}
+        updateTask={updateTask}
+        taskObj={taskObj}
+      />
+    </MUICard>
+  );
 };
 
 export default Card;
